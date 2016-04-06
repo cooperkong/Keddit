190d3c17bc96eec008d55ce0f9ce5e7a7c7828ec
Wenchao Kong <wenchao.kong@gmail.com>
2016-04-06 16:44:46 +1000

    use of Android cotillon extensions and delegate properties(lazy properties)

--- app/src/main/java/com/littlesword/keddit/NewsFragment.kt
+++ app/src/main/java/com/littlesword/keddit/NewsFragment.kt
@@ -3,24 +3,28 @@ package com.littlesword.keddit
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v7.widget.LinearLayoutManager
-import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
+import kotlinx.android.synthetic.main.news_fragment.*;
 /**
  * Created by kongw1 on 31/03/16.
  */
 class NewsFragment : Fragment() {
 
-    private var newsList: RecyclerView? = null
-
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
 //        val view = inflater?.inflate(R.layout.news_fragment, container, false)
-        newsList = view?.findViewById(R.id.news_list) as RecyclerView?
-        container?.inflate(R.layout.news_fragment);//use of extension functions
-        newsList?.setHasFixedSize(true) // use this setting to improve performance
-        newsList?.layoutManager = LinearLayoutManager(context)
-        return view
+        return container?.inflate(R.layout.news_fragment); //use of extension functions
+
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        newsList.setHasFixedSize(true)
+        newsList.layoutManager = LinearLayoutManager(context)
+    }
+    //lazy loading properties when needed to avoid null
+    private val newsList by lazy {
+        news_list
     }
 }
